0533e267c279dd647e6154c5b4640637aa53c195
WorldDogs <juglants@gmail.com>
2022-01-19 00:36:23 +0800

    add getLogs method

--- test/utils.js
+++ test/utils.js
@@ -5,7 +5,15 @@ async function getInstance(name, address) {
     return factory.attach(address);
 }
 
+async function getLogs(tx, eventName,name) {
+    let receipt = await tx.wait()
+    // todo check
+    let evt = receipt.events?.filter((x) => {return x.event == eventName})
+    // console.log(evt)
+    return evt[0].args[name]
+}
 
 module.exports = {
-    getInstance
+    getInstance,
+    getLogs
 }
